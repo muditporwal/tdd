7033f49bd18de45abf2f11e48c70004f69bec2ab
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-21 08:21:52 +0800

    Removed Stash

--- babysteps/gitReset.sh
+++ babysteps/gitReset.sh
@@ -3,7 +3,7 @@
 cd ~/WS/tdd || return 99
 
 if [ "$(git log --pretty=format:%ct -1)" -lt "$(date +%s -d '2 minutes ago')" ]; then
-  git stash;
+#  git stash; # Did not work since stash was cleared out because of the reset
   git reset --hard HEAD;
-  git stash list --since='1 hour ago' | cut -d: -f1 | xargs git stash drop
+#  git stash list --since='1 hour ago' | cut -d: -f1 | xargs git stash drop
 fi
