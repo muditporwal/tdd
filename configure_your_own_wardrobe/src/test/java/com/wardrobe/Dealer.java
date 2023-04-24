eab60cd70f0881d62342b7331956491f1cbf2cbd
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-24 08:47:51 +0800

    Added Dealer

---
+++ configure_your_own_wardrobe/src/test/java/com/wardrobe/Dealer.java
@@ -0,0 +1,10 @@
+package com.wardrobe;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Dealer {
+  public List<WardrobeElement> getCatalogue() {
+    return Collections.emptyList();
+  }
+}
--- configure_your_own_wardrobe/src/test/java/com/wardrobe/WardrobeTest.java
+++ configure_your_own_wardrobe/src/test/java/com/wardrobe/WardrobeTest.java
@@ -2,19 +2,26 @@ package com.wardrobe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class WardrobeTest {
 
   @Test
-  void checkWallIsOfLength() {
+  void checkWall() {
     Wall w = new Wall(250);
     assertEquals(250, w.lengthInCms());
   }
 
   @Test
-  void checkElementsOfWardrobe() {
+  void checkWardrobeElement() {
     WardrobeElement element50 = new WardrobeElement(50);
     assertEquals(50, element50.lengthInCms());
+  }
+
+  @Test
+  void checkDealerOptions() {
+    Dealer dealer = new Dealer();
+    List<WardrobeElement> catalogue = dealer.getCatalogue();
   }
 }
