a050f145461311394eb4b0dc9bef3088228a246e
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-25 08:25:40 +0800

    cleanup

--- configure_your_own_wardrobe/src/test/java/com/wardrobe/ElementPlannerTest.java
+++ configure_your_own_wardrobe/src/test/java/com/wardrobe/ElementPlannerTest.java
@@ -24,26 +24,6 @@ class ElementPlannerTest {
     assertEquals(4, planner.dealer().getCatalog().size());
   }
 
-  // @Test
-  void testCombinationGettingForWardrobes() {
-    assertNotNull(planner.getCombinations());
-    ArrayList<WardrobeElement> elements50 = new ArrayList<>();
-    elements50.add(new WardrobeElement(50));
-    elements50.add(new WardrobeElement(50));
-    elements50.add(new WardrobeElement(50));
-    elements50.add(new WardrobeElement(50));
-    elements50.add(new WardrobeElement(50));
-    ArrayList<WardrobeElement> elements50n100 = new ArrayList<>();
-    elements50n100.add(new WardrobeElement(100));
-    elements50n100.add(new WardrobeElement(100));
-    elements50n100.add(new WardrobeElement(50));
-
-    assertNotNull(planner.getCombinations());
-    assertTrue(planner.getCombinations().contains(elements50n100));
-    assertTrue(planner.getCombinations().contains(elements50));
-    assertEquals(50, planner.getCombinations().size());
-  }
-
   // Find out computation logic
   // Empty catalog - Done
   // Wall too small
