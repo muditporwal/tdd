be0a978b9a3395a2dfd29a7e99f6f8b1e864977f
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-27 07:33:40 +0800

    Moved some code around

--- configure_your_own_wardrobe/src/test/java/com/wardrobe/ElementPlannerTest.java
+++ configure_your_own_wardrobe/src/test/java/com/wardrobe/ElementPlannerTest.java
@@ -34,48 +34,4 @@ class ElementPlannerTest {
     ElementPlanner planner = new ElementPlanner(wall, dealer);
     assertThrows(IllegalStateException.class, planner::getCombinations, "Dealer has no catalog");
   }
-
-  @Test
-  void testWallTooSmall() {
-    Wall wall = new Wall(1);
-    Dealer dealer = new Dealer();
-    assertNotNull(dealer.getCatalog());
-    ElementPlanner planner = new ElementPlanner(wall, dealer);
-    assertThrows(IllegalStateException.class, planner::getCombinations, "Wall is too small");
-    // TODO review if illeagal state is what we want to throw
-  }
-
-  // One element fits wall
-  @Test
-  void testOneElementFitsWall() {
-    Wall wall = new Wall(10);
-    List<WardrobeElement> one = new ArrayList<>();
-    one.add(new WardrobeElement(10));
-    Dealer dealer = new Dealer(one);
-    assertNotNull(dealer.getCatalog());
-    assertEquals(1, dealer.getCatalog().size());
-    ElementPlanner planner = new ElementPlanner(wall, dealer);
-    assertEquals(1, planner.getCombinations().size());
-  }
-
-  @Test
-  void testOnlyOneElementFitsWall() {
-    Wall wall = new Wall(75);
-    Dealer dealer = new Dealer();
-    assertNotNull(dealer.getCatalog());
-    ElementPlanner planner = new ElementPlanner(wall, dealer);
-    assertEquals(1, planner.getCombinations().size());
-  }
-
-  @Test
-  void testTwoElementFitsWall() {
-    Wall wall = new Wall(125);
-    Dealer dealer = new Dealer();
-    assertNotNull(dealer.getCatalog());
-    ElementPlanner planner = new ElementPlanner(wall, dealer);
-    System.out.println(planner.getCombinations());
-    assertEquals(1, planner.getCombinations().size());
-  }
-
-  // remove duplicates
 }
