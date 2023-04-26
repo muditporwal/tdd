04ed69472ab2f6b5a8491d898491133963295554
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-26 11:29:39 +0800

    retracing steps

--- configure_your_own_wardrobe/src/main/java/com/wardrobe/ElementPlanner.java
+++ configure_your_own_wardrobe/src/main/java/com/wardrobe/ElementPlanner.java
@@ -10,26 +10,6 @@ public record ElementPlanner(Wall wall, Dealer dealer) {
       throw new IllegalStateException("Dealer has no catalog");
     }
     ArrayList<List<WardrobeElement>> combinations = new ArrayList<>();
-    return getCombinations(wall.lengthInCms(), combinations, catalog);
-  }
-
-  private ArrayList<List<WardrobeElement>> getCombinations(
-      int remainingLength,
-      ArrayList<List<WardrobeElement>> combinations,
-      List<WardrobeElement> catalog) {
-
-    for (WardrobeElement wardrobeElement : catalog) {
-      List<WardrobeElement> accumulator = new ArrayList<>();
-      if (remainingLength < wardrobeElement.lengthInCms()) {
-        continue;
-      } else {
-        remainingLength = remainingLength - wardrobeElement.lengthInCms();
-        accumulator.add(wardrobeElement);
-      }
-      if (remainingLength == 0) {
-        combinations.add(accumulator);
-      }
-    }
     return combinations;
   }
 }
