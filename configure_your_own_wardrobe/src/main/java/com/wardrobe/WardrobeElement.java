6b6e6235aa97a6c3a3a1f8a1a6df6757e0984c84
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-28 07:44:18 +0800

    Moved CostMap to static Context

--- configure_your_own_wardrobe/src/main/java/com/wardrobe/WardrobeElement.java
+++ configure_your_own_wardrobe/src/main/java/com/wardrobe/WardrobeElement.java
@@ -3,12 +3,17 @@ package com.wardrobe;
 import java.util.HashMap;
 
 public record WardrobeElement(int lengthInCms) {
-  public int costInUSD() {
-    HashMap<Integer, Integer> costMap = new HashMap<>();
+
+  private static final HashMap<Integer, Integer> costMap = new HashMap<>();
+
+  static {
     costMap.put(50, 59);
     costMap.put(75, 62);
     costMap.put(100, 90);
     costMap.put(120, 111);
+  }
+
+  public int costInUSD() {
     return costMap.getOrDefault(lengthInCms, 1000);
   }
 }
