c71d6b198eb57fca97ce7f8cf27f0a70dfd64b67
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-28 07:41:18 +0800

    SonarLint

--- configure_your_own_wardrobe/src/main/java/com/wardrobe/WardrobeElement.java
+++ configure_your_own_wardrobe/src/main/java/com/wardrobe/WardrobeElement.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public record WardrobeElement(int lengthInCms) {
   public int costInUSD() {
-    HashMap<Integer, Integer> costMap = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> costMap = new HashMap<>();
     costMap.put(50, 59);
     costMap.put(75, 62);
     costMap.put(100, 90);
