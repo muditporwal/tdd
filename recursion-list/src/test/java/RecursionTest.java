a6b5f52334086ff7ad866ba98ccd9969c623752b
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-29 11:22:47 +0800

    Commit;

--- recursion-list/src/test/java/RecursionTest.java
+++ recursion-list/src/test/java/RecursionTest.java
@@ -1,1 +1,11 @@
-public class RecursionTest {}
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class RecursionTest {
+
+  @Test
+  void testListCreation() {
+    List<String> low = new ArrayList<>();
+  }
+}
