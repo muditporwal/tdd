6bbd76806adbb4a574ead086c873c3181bee5729
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-04-29 11:32:50 +0800

    Added Failing Test Case

--- recursion-list/src/test/java/com/tdd/recursion/Recursion.java
+++ recursion-list/src/test/java/com/tdd/recursion/Recursion.java
@@ -3,7 +3,8 @@ package com.tdd.recursion;
 import java.util.List;
 
 public class Recursion {
-    public static List<String> reverse(List<String> low) {
-        return low;
-    }
+  public static List<String> reverse(List<String> low) {
+    if (low.size() <= 1) return low;
+    else return null;
+  }
 }
--- recursion-list/src/test/java/com/tdd/recursion/RecursionTest.java
+++ recursion-list/src/test/java/com/tdd/recursion/RecursionTest.java
@@ -9,9 +9,33 @@ import org.junit.jupiter.api.Test;
 public class RecursionTest {
 
   @Test
-  void testListCreation() {
+  void testEmptyListReversal() {
     List<String> low = new ArrayList<>();
     List<String> reverse = Recursion.reverse(low);
     assertEquals(low, reverse);
+  }
+
+  @Test
+  void testBaseCaseSingleItemListReversal() {
+    List<String> low = new ArrayList<>();
+    String thing = "hello";
+    low.add(thing);
+    List<String> reverse = Recursion.reverse(low);
+    assertEquals(low, reverse);
+  }
+
+  @Test
+  void testRecursiveCaseTwoItemListReversal() {
+    List<String> low = new ArrayList<>();
+    String hello = "hello";
+    String world = "world";
+    low.add(hello);
+    low.add(world);
+
+    List<String> expected = new ArrayList<>();
+    expected.add(world);
+    expected.add(hello);
+    List<String> reverse = Recursion.reverse(low);
+    assertEquals(expected, reverse);
   }
 }
