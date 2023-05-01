d636d12f0158415c81699a625222970ff12e7b15
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-02 07:08:18 +0800

    Setup TestCase

---
+++ recursion-list/src/test/java/ABRecord.java
@@ -0,0 +1,9 @@
+public class ABRecord {
+  public int getA() {
+    return 0;
+  }
+
+  public int getB() {
+    return 0;
+  }
+}
--- recursion-list/src/test/java/Recursion2Test.java
+++ recursion-list/src/test/java/Recursion2Test.java
@@ -1,1 +1,19 @@
-public class Recursion2Test {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Recursion2Test {
+
+  /*
+      i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+      ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+      iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+      a and b will both be lower than 10E8.
+  */
+  @Test
+  void testCreateABRecord() {
+    ABRecord a = new ABRecord();
+    assertEquals(0, a.getA());
+    assertEquals(0, a.getB());
+  }
+}
