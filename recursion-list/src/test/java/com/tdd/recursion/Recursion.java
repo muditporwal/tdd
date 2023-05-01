e3d0f9d67016727583e9a0b8629cc75189c744df
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-01 13:17:52 +0800

    Added first commit for recursion based list reversal

--- recursion-list/src/test/java/com/tdd/recursion/Recursion.java
+++ recursion-list/src/test/java/com/tdd/recursion/Recursion.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class Recursion {
   public static List<String> reverse(List<String> low) {
     if (low.size() <= 1) return low;
-    else return null;
+    else {
+      String head = low.get(0);
+      List<String> tail = low.subList(1, low.size() - 1);
+      reverse(tail).add(head);
+      return tail;
+    }
   }
 }
