c11b669f3e1264d89bc5597a57eae008683957f9
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-03 07:18:38 +0800

    Added toDigits Test Case

--- recursion-list/src/main/java/com/tdd/recursion/ex1/SumOddDigits.java
+++ recursion-list/src/main/java/com/tdd/recursion/ex1/SumOddDigits.java
@@ -1,3 +1,14 @@
 package com.tdd.recursion.ex1;
 
-public class SumOddDigits {}
+public class SumOddDigits {
+
+  private final int number;
+
+  public SumOddDigits(int number) {
+    this.number = number;
+  }
+
+  public int[] getDigits() {
+    return null;
+  }
+}
--- recursion-list/src/test/java/com/tdd/recursion/ex1/SumOddDigitsTest.java
+++ recursion-list/src/test/java/com/tdd/recursion/ex1/SumOddDigitsTest.java
@@ -8,7 +8,16 @@ class SumOddDigitsTest {
 
   @Test
   void testInitialization() {
-    SumOddDigits sod = new SumOddDigits();
+    SumOddDigits sod = new SumOddDigits(235);
     assertNotNull(sod);
+  }
+
+  @Test
+  void testConversionToDigits() {
+    SumOddDigits sod = new SumOddDigits(235);
+    int[] digits = sod.getDigits();
+    assertEquals(5, digits[0]);
+    assertEquals(3, digits[1]);
+    assertEquals(2, digits[2]);
   }
 }
