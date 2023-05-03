af7537664d9820e187fbb4e6812a1e3955967aa6
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-04 05:59:46 +0800

    Added Exception Handling

--- recursion-list/src/main/java/com/tdd/recursion/ninja/NinjaChirp.java
+++ recursion-list/src/main/java/com/tdd/recursion/ninja/NinjaChirp.java
@@ -1,3 +1,12 @@
 package com.tdd.recursion.ninja;
 
-public class NinjaChirp {}
+public class NinjaChirp {
+
+  public static final String CHIRP = "chirp";
+
+  public String canChirp(int i) {
+    if (i <= -1) throw new IllegalArgumentException("A ninja cannot understand this!");
+    if (i == 1) return CHIRP;
+    return null;
+  }
+}
--- recursion-list/src/test/java/com/tdd/recursion/ninja/NinjaChirpTest.java
+++ recursion-list/src/test/java/com/tdd/recursion/ninja/NinjaChirpTest.java
@@ -1,5 +1,8 @@
 package com.tdd.recursion.ninja;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
 import org.junit.jupiter.api.Test;
 
 public class NinjaChirpTest {
@@ -7,5 +10,18 @@ public class NinjaChirpTest {
   @Test
   void initializeNinjaChirp() {
     NinjaChirp ninja = new NinjaChirp();
+  }
+
+  @Test
+  void testIfNinjaCanChirp() {
+    NinjaChirp ninja = new NinjaChirp();
+    String chirpCode = ninja.canChirp(1);
+    assertEquals("chirp", chirpCode);
+  }
+
+  @Test
+  void testIfNinjaUnderstandsOddRequests() {
+    NinjaChirp ninja = new NinjaChirp();
+    assertThrowsExactly(IllegalArgumentException.class, () -> ninja.canChirp(-1));
   }
 }
