10e12d918d396ad7df2cf2792429b913bd4f2ba3
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-04 05:52:58 +0800

    Added Setup

---
+++ recursion-list/src/test/java/com/tdd/recursion/ninja/NinjaChirpTest.java
@@ -0,0 +1,11 @@
+package com.tdd.recursion.ninja;
+
+import org.junit.jupiter.api.Test;
+
+public class NinjaChirpTest {
+
+  @Test
+  void initializeNinjaChirp() {
+    NinjaChirp ninja = new NinjaChirp();
+  }
+}
