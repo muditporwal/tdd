4104ee9dcb138302ec0de65106bd3cbe883c7510
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-05 05:38:43 +0800

    Setup complete

--- recursion-list/src/test/java/com/tdd/recursion/factorial/FactorialTest.java
+++ recursion-list/src/test/java/com/tdd/recursion/factorial/FactorialTest.java
@@ -1,3 +1,17 @@
 package com.tdd.recursion.factorial;
 
-public class FactorialTest {}
+import org.junit.jupiter.api.Test;
+
+public class FactorialTest {
+
+  /*Spec :
+  The code is a recursive attempt in calculating the result of a factorial function.
+  I.e. the result of 3! (Factorial of 3) is 3x2x1 = 6.
+  In case of 0! the result is 1 and in case of a negative factorial the result should be 0.
+   */
+
+  @Test
+  void initFactorial() {
+    Factorial fact = new Factorial();
+  }
+}
