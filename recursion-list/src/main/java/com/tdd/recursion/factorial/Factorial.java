70ec630f25432540b1041ddd31d217b956d389e6
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-05 07:03:16 +0800

    Lint

--- recursion-list/src/main/java/com/tdd/recursion/factorial/Factorial.java
+++ recursion-list/src/main/java/com/tdd/recursion/factorial/Factorial.java
@@ -1,6 +1,9 @@
 package com.tdd.recursion.factorial;
 
 public class Factorial {
+  private Factorial() {
+  }
+
   public static int compute(int i) {
     if (i < 0)
       throw new IllegalArgumentException("Factorial cannot be calculated for negative numbers");
