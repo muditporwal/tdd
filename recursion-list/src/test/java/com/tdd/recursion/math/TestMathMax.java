3b0aa58f91f6dd14a7813d66892739541158cae7
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-10 05:29:29 +0800

    Added 2 element failing case

--- recursion-list/src/test/java/com/tdd/recursion/math/TestMathMax.java
+++ recursion-list/src/test/java/com/tdd/recursion/math/TestMathMax.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class TestMathMax {
 
   @Test
-  void singleDigitListShouldReturnTheDigit() {
+  void singleElementListShouldReturnTheDigit() {
     int[] a = new int[] {1};
     int b = MathMax.apply(null, a);
     assertEquals(1, b);
@@ -19,5 +19,12 @@ class TestMathMax {
     int[] a = new int[] {};
     int b = MathMax.apply(null, a);
     assertEquals(Integer.MIN_VALUE, b);
+  }
+
+  @Test
+  void twoElementListShouldReturnTheMax() {
+    int[] a = new int[] {1, 2};
+    int b = MathMax.apply(null, a);
+    assertEquals(2, b);
   }
 }
