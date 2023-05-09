2346da3258fb9db794350fc96ee7bb39235c52b9
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-10 05:25:06 +0800

    Added Single Element Test Case

--- recursion-list/src/main/java/com/tdd/recursion/MathMax.java
+++ recursion-list/src/main/java/com/tdd/recursion/MathMax.java
@@ -1,3 +1,8 @@
 package com.tdd.recursion;
 
-class MathMax {}
+public class MathMax {
+  public static int apply(Integer index, int[] a) {
+    if (a.length == 1) return a[0];
+    return Integer.MIN_VALUE;
+  }
+}
--- recursion-list/src/test/java/com/tdd/recursion/math/TestMathMax.java
+++ recursion-list/src/test/java/com/tdd/recursion/math/TestMathMax.java
@@ -1,5 +1,16 @@
 package com.tdd.recursion.math;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.tdd.recursion.MathMax;
+import org.junit.jupiter.api.Test;
+
 class TestMathMax {
-    
+
+  @Test
+  void singleDigitListShouldReturnTheDigit() {
+    int[] a = new int[] {1};
+    int b = MathMax.apply(null, a);
+    assertEquals(1, b);
+  }
 }
