2097f2cc825bef56560602551ab645bba47e3076
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-11 05:25:32 +0800

    Added test case with width 0

--- recursion-list/src/test/java/com/tdd/recursion/steps/TestStairs.java
+++ recursion-list/src/test/java/com/tdd/recursion/steps/TestStairs.java
@@ -1,3 +1,16 @@
 package com.tdd.recursion.steps;
 
-class TestStairs {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class TestStairs {
+
+  @Test
+  void testWidth0ShouldReturnEmptyArray() {
+    Stairs stairs = Stairs.width(0).build();
+    List<Integer> steps = stairs.traverse();
+    assertEquals(0, steps.size());
+  }
+}
