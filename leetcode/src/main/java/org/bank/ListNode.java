95093cc7cfd2ad9be78db3563617013db37c6908
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-15 07:58:39 +0800

    Added LinkedList Setup

---
+++ leetcode/src/main/java/org/bank/ListNode.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+abstract class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
---
+++ leetcode/src/test/java/org/bank/LinkedListTest.java
@@ -0,0 +1,11 @@
+package org.bank;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest {
+
+  @Test
+  void testSetupLinkedList() {
+    ListNode node = new LinkedListNode();
+  }
+}
