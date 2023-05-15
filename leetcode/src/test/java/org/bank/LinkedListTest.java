e590d1db2a352c20c4bb75a9e20a7fb315b21446
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-15 08:22:00 +0800

    Clearing up interface for the implementation

--- leetcode/src/test/java/org/bank/LinkedListTest.java
+++ leetcode/src/test/java/org/bank/LinkedListTest.java
@@ -2,6 +2,7 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LinkedListTest {
@@ -11,6 +12,13 @@ class LinkedListTest {
     LinkedList list = new LinkedList();
     ListNode e = new ListNode(1);
     list.elements.add(e);
-    assertEquals(list.elements, list.reverseList(e));
+    List<ListNode> reverseList = list.reverseList(e);
+    assertEquals(reverseList.get(0), e);
+    ListNode f = new ListNode(2);
+    e.next = f;
+    list.elements.add(f);
+    reverseList = list.reverseList(e);
+    assertEquals(reverseList.get(0), f);
+    assertEquals(reverseList.get(1), e);
   }
 }
