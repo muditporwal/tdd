92070323efc2d48a10a4a2f257e2027d77fba3c9
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-17 07:38:00 +0800

    Fixed Test Case with base case of 0

--- leetcode/src/main/java/org/bank/Fibonacci.java
+++ leetcode/src/main/java/org/bank/Fibonacci.java
@@ -2,7 +2,8 @@ package org.bank;
 
 class Fibonacci {
   public int fib(int i) {
-    if (i == 1 || i == 0) return 1;
+    if (i == 1) return 1;
+    if (i == 0) return 0;
     else return 0;
   }
 }
--- leetcode/src/test/java/org/bank/FibonacciTest.java
+++ leetcode/src/test/java/org/bank/FibonacciTest.java
@@ -2,23 +2,33 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FibonacciTest {
 
+  private Fibonacci fib;
+
+  @BeforeEach
+  void setup() {
+    fib = new Fibonacci();
+  }
+
   @Test
   void fibonacciWithBaseCaseOfZeroShouldReturn1() {
-    Fibonacci fib = new Fibonacci();
     int val = fib.fib(0);
-    assertEquals(1, val);
-    // public int fib(int n)
+    assertEquals(0, val);
   }
 
   @Test
   void fibonacciWithBaseCaseOfOneShouldReturn1() {
-    Fibonacci fib = new Fibonacci();
     int val = fib.fib(1);
     assertEquals(1, val);
-    // public int fib(int n)
+  }
+
+  @Test
+  void fibonacciWithHigherOrderIntegerShouldReturnSumOfPreviousFibs() {
+    int val = fib.fib(2);
+    assertEquals(2, val);
   }
 }
