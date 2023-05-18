89db7be0c0bbf077507863e040f8b4744019974a
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-19 07:32:18 +0800

    Added test for single element

--- leetcode/src/main/java/org/bank/LinkedListDelete.java
+++ leetcode/src/main/java/org/bank/LinkedListDelete.java
@@ -3,6 +3,8 @@ package org.bank;
 class LinkedListDelete {
   public static ListNode removeElements(ListNode head, int i) {
     if (head == null) return null;
-    return null;
+    ListNode newHead = head;
+    if(head.val == i) newHead = head.next;
+    return newHead;
   }
 }
--- leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
+++ leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
@@ -2,14 +2,22 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LinkedListDeleteTest {
 
   @Test
-  void testSetupOfDeleteInterface() {
+  void deletionFromAnEmptyListShouldReturnAnEmptyList() {
     // ListNode head = ListNode.build(List.of(1));
     ListNode newHead = LinkedListDelete.removeElements(null, 1);
+    assertNull(newHead);
+  }
+
+  @Test
+  void deletionFromAnSingleElementListShouldReturnAnEmptyList() {
+    ListNode head = ListNode.build(List.of(1));
+    ListNode newHead = LinkedListDelete.removeElements(head, 1);
     assertNull(newHead);
   }
 }
