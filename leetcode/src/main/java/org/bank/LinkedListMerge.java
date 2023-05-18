d061fc746431459f39602d39763d7bf2b7ffd0ac
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-18 10:41:07 +0800

    Added Single Element Test Case

--- leetcode/src/main/java/org/bank/LinkedListMerge.java
+++ leetcode/src/main/java/org/bank/LinkedListMerge.java
@@ -5,6 +5,15 @@ class LinkedListMerge {
     if (n1 == null && n2 == null) throw new IllegalStateException("both nodes should not be null");
     if (n1 == null && n2 != null) return n2;
     if (n2 == null && n1 != null) return n1;
+    if (n1 != null && n1.next == null && n2 != null && n2.next == null) {
+      if (n1.val <= n2.val) {
+        n1.next = n2;
+        return n1;
+      } else {
+        n2.next = n1;
+        return n2;
+      }
+    }
     return null;
   }
 }
