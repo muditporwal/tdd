279f55a3ac123d200f5747a0667fa5408be21320
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-18 10:59:19 +0800

    Refactored and CleanUp Code

--- leetcode/src/main/java/org/bank/LinkedListMerge.java
+++ leetcode/src/main/java/org/bank/LinkedListMerge.java
@@ -1,27 +1,19 @@
 package org.bank;
 
 class LinkedListMerge {
+  private LinkedListMerge() {}
+
   public static ListNode mergeTwoLists(ListNode n1, ListNode n2) {
     if (n1 == null && n2 == null) throw new IllegalStateException("both nodes should not be null");
-    if (n1 == null && n2 != null) return n2;
-    if (n2 == null && n1 != null) return n1;
-    if (n1 != null && n1.next == null && n2 != null && n2.next == null) {
-      if (n1.val <= n2.val) {
-        n1.next = n2;
-        return n1;
-      } else {
-        n2.next = n1;
-        return n2;
-      }
-    }
-    if (n1 != null && n2 != null) {
+    else if (n1 == null) return n2;
+    else if (n2 == null) return n1;
+    else {
       if (n1.val <= n2.val) {
         return getListNode(n1, n2);
       } else {
         return getListNode(n2, n1);
       }
     }
-    return null;
   }
 
   private static ListNode getListNode(ListNode n1, ListNode n2) {
