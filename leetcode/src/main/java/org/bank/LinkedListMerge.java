40ed90f9f3b190b401bd39903c3d94281cec76cb
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-18 09:29:55 +0800

    Throwing Exception

--- leetcode/src/main/java/org/bank/LinkedListMerge.java
+++ leetcode/src/main/java/org/bank/LinkedListMerge.java
@@ -1,5 +1,12 @@
 package org.bank;
 
 class LinkedListMerge {
-  public static ListNode mergeTwoLists(ListNode o, ListNode o1) {}
+  public static ListNode mergeTwoLists(ListNode n1, ListNode n2) {
+    if (n1 == null) {
+      if (n2 == null) {
+        throw new IllegalStateException("both nodes should not be null");
+      }
+    }
+    return null;
+  }
 }
