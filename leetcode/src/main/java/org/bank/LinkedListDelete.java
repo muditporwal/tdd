0de98e0265bdd6255ad356dfb948b5dba704e051
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-19 07:36:56 +0800

    Added failing testcase

--- leetcode/src/main/java/org/bank/LinkedListDelete.java
+++ leetcode/src/main/java/org/bank/LinkedListDelete.java
@@ -4,7 +4,11 @@ class LinkedListDelete {
   public static ListNode removeElements(ListNode head, int i) {
     if (head == null) return null;
     ListNode newHead = head;
-    if(head.val == i) newHead = head.next;
+    if (head.val == i) {
+      newHead = removeElements(head.next, i);
+    } else {
+      return null;
+    }
     return newHead;
   }
 }
--- leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
+++ leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
@@ -1,6 +1,6 @@
 package org.bank;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -19,5 +19,20 @@ class LinkedListDeleteTest {
     ListNode head = ListNode.build(List.of(1));
     ListNode newHead = LinkedListDelete.removeElements(head, 1);
     assertNull(newHead);
+  }
+
+  @Test
+  void deletionFromATwoElementListShouldRemoveAllElementsWithValueAndReturn() {
+    ListNode head = ListNode.build(List.of(1, 1));
+    ListNode newHead = LinkedListDelete.removeElements(head, 1);
+    assertNull(newHead);
+  }
+
+  @Test
+  void deletionFromATwoElementListShouldRemoveTheElementsWithValueAndReturn() {
+    ListNode head = ListNode.build(List.of(1, 2, 1));
+    ListNode newHead = LinkedListDelete.removeElements(head, 1);
+    assertNotNull(newHead);
+    assertEquals(new ListNode(2), newHead);
   }
 }
