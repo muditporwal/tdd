ddeaafd85127378164e1aae9f78eafccbfa44bd1
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-19 07:28:54 +0800

    Added Null Test

---
+++ leetcode/src/main/java/org/bank/LinkedListDelete.java
@@ -0,0 +1,8 @@
+package org.bank;
+
+class LinkedListDelete {
+  public static ListNode removeElements(ListNode head, int i) {
+    if (head == null) return null;
+    return null;
+  }
+}
--- leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
+++ leetcode/src/test/java/org/bank/LinkedListDeleteTest.java
@@ -1,11 +1,15 @@
 package org.bank;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 
 class LinkedListDeleteTest {
 
-    @Test
-    void testSetupOfDeleteInterface(){
-        
-    }
+  @Test
+  void testSetupOfDeleteInterface() {
+    // ListNode head = ListNode.build(List.of(1));
+    ListNode newHead = LinkedListDelete.removeElements(null, 1);
+    assertNull(newHead);
+  }
 }
