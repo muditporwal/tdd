1ad83cebac2c61a8b475935261181b5cb814a9b7
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-19 08:13:58 +0800

    Completed test cases

--- leetcode/src/main/java/org/bank/LinkedListDelete.java
+++ leetcode/src/main/java/org/bank/LinkedListDelete.java
@@ -3,11 +3,12 @@ package org.bank;
 class LinkedListDelete {
   public static ListNode removeElements(ListNode head, int i) {
     if (head == null) return null;
-    ListNode newHead = head;
+    ListNode newHead;
     if (head.val == i) {
       newHead = removeElements(head.next, i);
     } else {
-      return null;
+      head.next = removeElements(head.next, i);
+      newHead = head;
     }
     return newHead;
   }
