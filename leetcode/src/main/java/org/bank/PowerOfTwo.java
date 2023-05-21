7428e63cbb8c65aa8cdb50a3474435c5ed50e637
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-22 07:21:42 +0800

    Updated Test cases for negative values;

--- leetcode/src/main/java/org/bank/PowerOfTwo.java
+++ leetcode/src/main/java/org/bank/PowerOfTwo.java
@@ -2,6 +2,8 @@ package org.bank;
 
 class PowerOfTwo {
   public static Boolean isPowerOfTwo(int i) {
-      return null;
+    if (i <= 0) throw new IllegalArgumentException("value must be greater than 0");
+    else if (i == 1) return true;
+    return null;
   }
 }
--- leetcode/src/test/java/org/bank/PowerOfTwoTest.java
+++ leetcode/src/test/java/org/bank/PowerOfTwoTest.java
@@ -1,14 +1,21 @@
 package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import org.junit.jupiter.api.Test;
 
 class PowerOfTwoTest {
 
   @Test
-  void setupTest() {
+  void testForEdgeCaseOfOne() {
     Boolean result = PowerOfTwo.isPowerOfTwo(1);
     assertEquals(true, result);
+  }
+
+  @Test
+  void testForEdgeCaseOfZeroAndNegative() {
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(-1));
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(0));
   }
 }
