b1a81fa27ac963706f246cd0aad586cdc13789fc
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-23 11:09:41 +0800

    Cloned Power of Two to Power of Three

---
+++ leetcode/src/test/java/org/bank/PowerOfThreeTest.java
@@ -0,0 +1,42 @@
+package org.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PowerOfThreeTest {
+
+  @Test
+  void testForEdgeCaseOfOne() {
+    Boolean result = PowerOfTwo.isPowerOfTwo(1);
+    assertEquals(true, result);
+  }
+
+  @Test
+  void testForEdgeCaseOfZeroAndNegative() {
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(-1));
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(0));
+  }
+
+  @Test
+  void testForOddValues() {
+    assertEquals(false, PowerOfTwo.isPowerOfTwo(3));
+    assertEquals(false, PowerOfTwo.isPowerOfTwo(11));
+  }
+
+  @Test
+  void testForEvenValues() {
+    assertEquals(false, PowerOfTwo.isPowerOfTwo(6));
+    assertEquals(true, PowerOfTwo.isPowerOfTwo(8));
+  }
+
+  @Test
+  void testForEdgeCasesOf2Pow30() {
+    int pow30 = (int) Math.pow(2, 30);
+    assertTrue(pow30 > 0);
+    assertEquals(true, PowerOfTwo.isPowerOfTwo(pow30));
+    int pow31minus1 = (int) (Math.pow(2, 31)) - 1;
+    assertTrue(pow31minus1 > 0);
+    assertEquals(false, PowerOfTwo.isPowerOfTwo(pow31minus1));
+  }
+}
