8b493a98a24dee12ec9b54f4d461add651e31b6a
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-23 11:13:28 +0800

    Added Power of Three

---
+++ leetcode/src/main/java/org/bank/PowerOfThree.java
@@ -0,0 +1,10 @@
+package org.bank;
+
+class PowerOfThree {
+  public static boolean isPowerOfThree(int i) {
+    if (i <= 0) throw new IllegalArgumentException("value must be greater than 0");
+    else if (i == 1) return true;
+    else if (i % 3 != 0) return false;
+    else return isPowerOfThree(i / 3);
+  }
+}
--- leetcode/src/test/java/org/bank/PowerOfThreeTest.java
+++ leetcode/src/test/java/org/bank/PowerOfThreeTest.java
@@ -8,35 +8,35 @@ class PowerOfThreeTest {
 
   @Test
   void testForEdgeCaseOfOne() {
-    Boolean result = PowerOfTwo.isPowerOfTwo(1);
+    Boolean result = PowerOfThree.isPowerOfThree(1);
     assertEquals(true, result);
   }
 
   @Test
   void testForEdgeCaseOfZeroAndNegative() {
-    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(-1));
-    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfTwo.isPowerOfTwo(0));
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfThree.isPowerOfThree(-1));
+    assertThrowsExactly(IllegalArgumentException.class, () -> PowerOfThree.isPowerOfThree(0));
   }
 
   @Test
-  void testForOddValues() {
-    assertEquals(false, PowerOfTwo.isPowerOfTwo(3));
-    assertEquals(false, PowerOfTwo.isPowerOfTwo(11));
+  void testForNonMultipleValues() {
+    assertFalse(PowerOfThree.isPowerOfThree(2));
+    assertFalse(PowerOfThree.isPowerOfThree(10));
   }
 
   @Test
-  void testForEvenValues() {
-    assertEquals(false, PowerOfTwo.isPowerOfTwo(6));
-    assertEquals(true, PowerOfTwo.isPowerOfTwo(8));
+  void testForMultipleValues() {
+    assertFalse(PowerOfThree.isPowerOfThree(6));
+    assertTrue(PowerOfThree.isPowerOfThree(9));
   }
 
   @Test
   void testForEdgeCasesOf2Pow30() {
     int pow30 = (int) Math.pow(2, 30);
     assertTrue(pow30 > 0);
-    assertEquals(true, PowerOfTwo.isPowerOfTwo(pow30));
+    assertFalse(PowerOfThree.isPowerOfThree(pow30));
     int pow31minus1 = (int) (Math.pow(2, 31)) - 1;
     assertTrue(pow31minus1 > 0);
-    assertEquals(false, PowerOfTwo.isPowerOfTwo(pow31minus1));
+    assertFalse(PowerOfThree.isPowerOfThree(pow31minus1));
   }
 }
