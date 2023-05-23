9d68392a4fc1cd7efcc21db9d3452d9c08e9af7f
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-23 11:14:17 +0800

    Lint

--- leetcode/src/main/java/org/bank/PowerOfThree.java
+++ leetcode/src/main/java/org/bank/PowerOfThree.java
@@ -1,6 +1,8 @@
 package org.bank;
 
 class PowerOfThree {
+  private PowerOfThree() {}
+
   public static boolean isPowerOfThree(int i) {
     if (i <= 0) throw new IllegalArgumentException("value must be greater than 0");
     else if (i == 1) return true;
