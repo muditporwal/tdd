f60f67dc309c33da276b8fa1b4927b312fd75026
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-24 08:24:54 +0800

    Added simple addition

---
+++ leetcode/src/main/java/org/bank/AddTwoNumbers.java
@@ -0,0 +1,7 @@
+package org.bank;
+
+class AddTwoNumbers {
+  public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+    return null;
+  }
+}
--- leetcode/src/test/java/org/bank/AddTwoNumbersTest.java
+++ leetcode/src/test/java/org/bank/AddTwoNumbersTest.java
@@ -1,3 +1,17 @@
 package org.bank;
 
-class AddTwoNumbersTest {}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AddTwoNumbersTest {
+
+  @Test
+  void testSimpleSingleValueNodeAdditionShouldReturnSum() {
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode output = AddTwoNumbers.addTwoNumbers(node1, node2);
+    assertEquals(output, node3);
+  }
+}
