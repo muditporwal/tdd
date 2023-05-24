93c2c22e888a44f9423c05752570eedd3cb851fb
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-25 07:26:10 +0800

    Added Failing Test

--- leetcode/src/test/java/org/bank/SwapPairsTest.java
+++ leetcode/src/test/java/org/bank/SwapPairsTest.java
@@ -21,4 +21,20 @@ class SwapPairsTest {
     ListNode out = SwapPairs.swapPairs(head);
     assertEquals(expected, out);
   }
+
+  @Test
+  void testSwapWithThreeNode() {
+    ListNode head = ListNode.build(List.of(1, 2, 3));
+    ListNode expected = ListNode.build(List.of(2, 1, 3));
+    ListNode out = SwapPairs.swapPairs(head);
+    assertEquals(expected, out);
+  }
+
+  @Test
+  void testSwapWithFourNode() {
+    ListNode head = ListNode.build(List.of(1, 2, 3, 4));
+    ListNode expected = ListNode.build(List.of(2, 1, 4, 3));
+    ListNode out = SwapPairs.swapPairs(head);
+    assertEquals(expected, out);
+  }
 }
