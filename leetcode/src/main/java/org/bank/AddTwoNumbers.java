9ebca20edcba508abd016f1e6bf323691c9ed985
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-24 08:26:18 +0800

    Added simple addition

--- leetcode/src/main/java/org/bank/AddTwoNumbers.java
+++ leetcode/src/main/java/org/bank/AddTwoNumbers.java
@@ -2,6 +2,6 @@ package org.bank;
 
 class AddTwoNumbers {
   public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
-    return null;
+    return new ListNode(node1.val + node2.val);
   }
 }
