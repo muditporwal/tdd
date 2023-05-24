557c7b088603f01248aa2741b0f55a3867667e89
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-25 07:14:26 +0800

    Added singlenode swap test case

--- leetcode/src/test/java/org/bank/SwapPairsTest.java
+++ leetcode/src/test/java/org/bank/SwapPairsTest.java
@@ -1,12 +1,15 @@
 package org.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class SwapPairsTest {
 
   @Test
-  void testSetup() {
-    ListNode head = null;
-    ListNode newHead = SwapPairs.swapPairs(head);
+  void testSwapWithOneNode() {
+    ListNode head = new ListNode(1);
+    ListNode out = SwapPairs.swapPairs(head);
+    assertEquals(head, out);
   }
 }
