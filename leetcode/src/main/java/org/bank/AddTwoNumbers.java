b9c25fb20d604921c30a7c47e7d7e3be37b2a4dd
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-24 09:42:33 +0800

    Fixed implementation with Carry

--- leetcode/src/main/java/org/bank/AddTwoNumbers.java
+++ leetcode/src/main/java/org/bank/AddTwoNumbers.java
@@ -8,15 +8,20 @@ class AddTwoNumbers {
   }
 
   private static ListNode sumWithCarry(ListNode node1, ListNode node2, int carry) {
-    if (node1 == null) return ListNode.clone(node2);
-    if (node2 == null) return ListNode.clone(node1);
-    int sum = node1.val + node2.val;
-    sum = sum % 10;
+    if (node1 == null && carry == 0) return ListNode.clone(node2);
+    if (node2 == null && carry == 0) return ListNode.clone(node1);
+    int sum = normalizeVal(node1) + normalizeVal(node2) + carry;
     int carryFwd = sum / 10;
+    sum = sum % 10;
     ListNode head = new ListNode(sum);
     if (node1.next != null || node2.next != null) {
       head.next = sumWithCarry(node1.next, node2.next, carryFwd);
     }
     return head;
+  }
+
+  private static int normalizeVal(ListNode node) {
+    if (node == null) return 0;
+    else return node.val;
   }
 }
