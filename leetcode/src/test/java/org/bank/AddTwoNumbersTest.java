0e9f08206c90f2fc2f67e4ac62d4aa49ca7a23b3
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-24 08:29:49 +0800

    Added Test Case for multiple element addition

--- leetcode/src/test/java/org/bank/AddTwoNumbersTest.java
+++ leetcode/src/test/java/org/bank/AddTwoNumbersTest.java
@@ -2,15 +2,25 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class AddTwoNumbersTest {
 
   @Test
-  void testSimpleSingleValueNodeAdditionShouldReturnSum() {
+  void testNoCarrySingleValueNodeAdditionShouldReturnSum() {
     ListNode node1 = new ListNode(1);
     ListNode node2 = new ListNode(2);
     ListNode node3 = new ListNode(3);
+    ListNode output = AddTwoNumbers.addTwoNumbers(node1, node2);
+    assertEquals(output, node3);
+  }
+
+  @Test
+  void testNoCarryMultiNodeAdditionShouldReturnSum() {
+    ListNode node1 = ListNode.build(List.of(1, 2, 1));
+    ListNode node2 = ListNode.build(List.of(3, 2, 1));
+    ListNode node3 = ListNode.build(List.of(4, 4, 2));
     ListNode output = AddTwoNumbers.addTwoNumbers(node1, node2);
     assertEquals(output, node3);
   }
