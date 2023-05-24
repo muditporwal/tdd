65a8f3d5d858225642b1fea580b12a352f3d3b0a
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-24 08:40:46 +0800

    Added Two Number Test Behaviour for Multi Node with no carry

--- leetcode/src/main/java/org/bank/AddTwoNumbers.java
+++ leetcode/src/main/java/org/bank/AddTwoNumbers.java
@@ -1,10 +1,15 @@
 package org.bank;
 
 class AddTwoNumbers {
-    private AddTwoNumbers() {
-    }
+  private AddTwoNumbers() {}
 
-    public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
-    return new ListNode(node1.val + node2.val);
+  public static ListNode addTwoNumbers(ListNode node1, ListNode node2) {
+    if (node1 == null) return node2;
+    if (node2 == null) return node1;
+    ListNode head = new ListNode(node1.val + node2.val);
+    if (node1.next != null || node2.next != null) {
+      head.next = addTwoNumbers(node1.next, node2.next);
+    }
+    return head;
   }
 }
