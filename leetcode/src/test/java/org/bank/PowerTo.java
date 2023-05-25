955f7395d09c99220d81b74d1f4cf596d6adfb7c
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-26 05:25:42 +0800

    Added cases with Zero Power an Zero Operand

--- leetcode/src/test/java/org/bank/PowerTo.java
+++ leetcode/src/test/java/org/bank/PowerTo.java
@@ -2,6 +2,7 @@ package org.bank;
 
 class PowerTo {
   public static double myPow(double v, int i) {
+    if(i==0) return 1;
     return 0;
   }
 }
--- leetcode/src/test/java/org/bank/PowerToTest.java
+++ leetcode/src/test/java/org/bank/PowerToTest.java
@@ -1,11 +1,20 @@
 package org.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class PowerToTest {
 
-    @Test
-    void setup(){
-        double result = PowerTo.myPow(1.0,2);
-    }
+  @Test
+  void testPowOfZero() {
+    double result = PowerTo.myPow(1.0, 0);
+    assertEquals(1.0, result);
+  }
+
+  @Test
+  void testZeroPowersShouldReturnZero() {
+    double result = PowerTo.myPow(0.0, 1);
+    assertEquals(0.0, result);
+  }
 }
