b5df7900c120e2468aead4a033f0cf3ce3a89bcc
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-29 08:47:58 +0800

    Added three element test case

--- leetcode/src/test/java/org/bank/ReorderListTest.java
+++ leetcode/src/test/java/org/bank/ReorderListTest.java
@@ -2,15 +2,32 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ReorderListTest {
 
   @Test
   void singleElementReversalTestShouldReturnTheSameElement() {
-    ListNode single = new ListNode(1);
+    ListNode input = new ListNode(1);
     ListNode expected = new ListNode(1);
-    ReorderList.reorderList(single);
-    assertEquals(expected, single);
+    ReorderList.reorderList(input);
+    assertEquals(expected, input);
+  }
+
+  @Test
+  void twoElementReversalTestShouldReturnTheSameElements() {
+    ListNode input = ListNode.build(List.of(1, 2));
+    ListNode expected = ListNode.build(List.of(1, 2));
+    ReorderList.reorderList(input);
+    assertEquals(expected, input);
+  }
+
+  @Test
+  void threeElementReversalTestShouldReturnTheSameElements() {
+    ListNode input = ListNode.build(List.of(1, 2, 3));
+    ListNode expected = ListNode.build(List.of(1, 3, 2));
+    ReorderList.reorderList(input);
+    assertEquals(expected, input);
   }
 }
