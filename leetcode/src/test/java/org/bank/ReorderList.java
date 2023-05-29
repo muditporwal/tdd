47b8b6b29114399722c2f2af58cb2f28d7e9d0e6
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-29 09:10:47 +0800

    Added recursive implementation for list reversal

--- leetcode/src/test/java/org/bank/ReorderList.java
+++ leetcode/src/test/java/org/bank/ReorderList.java
@@ -5,6 +5,23 @@ class ReorderList {
   private ReorderList() {}
 
   public static void reorderList(ListNode head) {
-    if (head == null || head.next == null) {}
+    if (head == null || head.next == null) {
+    } else {
+      ListNode current = head;
+      ListNode tail = detachLastNode(current);
+      ListNode next = current.next;
+      current.next = tail;
+      tail.next = next;
+    }
+  }
+
+  private static ListNode detachLastNode(ListNode current) {
+    if (current.next.next == null) {
+      ListNode detach = current.next;
+      current.next = null;
+      return detach;
+    } else {
+      return detachLastNode(current.next);
+    }
   }
 }
