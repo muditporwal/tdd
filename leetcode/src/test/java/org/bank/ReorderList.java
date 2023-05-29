c7cf18e5700c5973aa5894a1619071a281862b2d
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-05-29 08:57:31 +0800

    Added empty conditional to isolate code flow

--- leetcode/src/test/java/org/bank/ReorderList.java
+++ leetcode/src/test/java/org/bank/ReorderList.java
@@ -4,5 +4,7 @@ class ReorderList {
 
   private ReorderList() {}
 
-  public static void reorderList(ListNode head) {}
+  public static void reorderList(ListNode head) {
+    if (head == null || head.next == null) {}
+  }
 }
