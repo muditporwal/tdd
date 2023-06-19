2d360fae46854597ad612a959441e91db2a59d98
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-06-19 13:46:43 +0800

    refactored and simplified interface

--- leetcode/src/main/java/org/bank/RoadTripAltitudeGain.java
+++ leetcode/src/main/java/org/bank/RoadTripAltitudeGain.java
@@ -4,8 +4,12 @@ class RoadTripAltitudeGain {
   public static int largestAltitude(int[] gain) {
     if (gain.length == 0) return 0;
     else {
-        if(gain[0] < 0) return 0;
-        else return gain[0];
+      return largestAltitudeWithAccumulation(gain, 0);
     }
+  }
+
+  private static int largestAltitudeWithAccumulation(int[] gain, int largestAltitude) {
+    if (gain[0] > 0) return gain[0];
+    else return 0;
   }
 }
