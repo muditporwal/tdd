7e48785ce6f240a120ad4936d4b015f742d55248
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-06-21 16:35:13 +0800

    Added Empty expression test case

---
+++ leetcode/src/test/java/org/bank/ParenthesisPermutations.java
@@ -0,0 +1,10 @@
+package org.bank;
+
+import java.util.Collections;
+import java.util.List;
+
+class ParenthesisPermutations {
+  public static List<Integer> diffWaysToCompute(String expression) {
+    return Collections.emptyList();
+  }
+}
--- leetcode/src/test/java/org/bank/ParenthesisPermutationsTest.java
+++ leetcode/src/test/java/org/bank/ParenthesisPermutationsTest.java
@@ -2,13 +2,14 @@ package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ParenthesisPermutationsTest {
 
   @Test
   void testEmptyExpressionShouldReturnEmptyArray() {
-    int[] output = ParenthesisPermutations.diffWaysToCompute("");
-    assertEquals(0, output.length);
+    List<Integer> output = ParenthesisPermutations.diffWaysToCompute("");
+    assertEquals(0, output.size());
   }
 }
