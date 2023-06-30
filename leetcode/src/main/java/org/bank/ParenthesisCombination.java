e59a20e1ca868aaffcc75dc74989a4b7811b1dc5
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-06-30 08:24:02 +0800

    Added Implementation

--- leetcode/src/main/java/org/bank/ParenthesisCombination.java
+++ leetcode/src/main/java/org/bank/ParenthesisCombination.java
@@ -17,4 +17,45 @@ class ParenthesisCombination {
   private void setExpression(String expression) {
     this.expression = expression;
   }
+
+  private interface Expression {
+    Integer evaluate();
+  }
+
+  private class Number implements Expression {
+
+    int value;
+
+    public Number(int value) {
+      this.value = value;
+    }
+
+    @Override
+    public Integer evaluate() {
+      return this.value;
+    }
+  }
+
+  private class Operator implements Expression {
+
+      private final Expression left;
+      private final Expression right;
+      private final String op;
+
+      public Operator(Expression left, Expression right, String op) {
+          this.left = left;
+          this.right = right;
+          this.op = op;
+      }
+
+      @Override
+    public Integer evaluate() {
+      return switch (this.op) {
+          case "+" -> left.evaluate() + right.evaluate();
+          case "-" -> left.evaluate() - right.evaluate();
+          case "*" -> left.evaluate() * right.evaluate();
+          default -> throw new IllegalStateException("Unexpected value: " + this.op);
+      };
+    }
+  }
 }
