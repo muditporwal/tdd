9d8eba3cc72c1c871866e29c3e66705dee300413
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-06-30 08:05:41 +0800

    Created Interface

--- leetcode/src/test/java/org/bank/ParenthesisCombinationTest.java
+++ leetcode/src/test/java/org/bank/ParenthesisCombinationTest.java
@@ -1,3 +1,20 @@
 package org.bank;
 
-class ParenthesisCombinationTest {}
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ParenthesisCombinationTest {
+  @Test
+  void createInterface() {
+    ParenthesisCombination pc = new ParenthesisCombination();
+    List<Integer> outcomes = pc.diffWaysToCompute("");
+  }
+
+  private class ParenthesisCombination {
+    public ParenthesisCombination() {}
+
+    public List<Integer> diffWaysToCompute(String s) {
+      return null;
+    }
+  }
+}
