dd9849ba37d3fbb1a2561757d08afdb722c8fc7c
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-04 07:17:37 +0800

    Testing Big String Scenarios

--- leetcode/src/test/java/org/bank/BuddyString.java
+++ leetcode/src/test/java/org/bank/BuddyString.java
@@ -3,6 +3,23 @@ package org.bank;
 public class BuddyString {
   public static boolean buddyStrings(String s, String goal) {
     if (s.isEmpty() || s.length() == 1) return false;
-    else return s.length() == goal.length();
+    else if (s.length() != goal.length()) return false;
+    else {
+      for (int i = 0; i < s.length(); i++) {
+        for (int j = 0; j < s.length(); j++) {
+          if (i != j){
+            char[] sChar = s.toCharArray();
+            char swap = sChar[i];
+            sChar[i] = sChar[j];
+            sChar[j] = swap;
+            String swapGoal = String.copyValueOf(sChar);
+            if (swapGoal.equals(goal)) {
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    }
   }
 }
--- leetcode/src/test/java/org/bank/BuddyStringTest.java
+++ leetcode/src/test/java/org/bank/BuddyStringTest.java
@@ -1,6 +1,7 @@
 package org.bank;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,19 @@ class BuddyStringTest {
   @Test
   void whenInputStringsLengthMatchAndLengthIsLessThan2_ReturnFalse() {
     assertFalse(BuddyString.buddyStrings("a", "a"));
+  }
+
+  @Test
+  void whenInputStringIs2Characters_CheckAndReturn() {
+    assertFalse(BuddyString.buddyStrings("ac", "ad"));
+    assertTrue(BuddyString.buddyStrings("aa", "aa"));
+    assertTrue(BuddyString.buddyStrings("ab", "ba"));
+  }
+
+  @Test
+  void whenInputStringIsLarge_CheckAndReturn() {
+    assertTrue(BuddyString.buddyStrings("acdc", "acdc"));
+    assertTrue(BuddyString.buddyStrings("abba", "baab"));
+    assertTrue(BuddyString.buddyStrings("fab", "baf"));
   }
 }
