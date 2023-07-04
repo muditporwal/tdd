b1a1627489ed5563a0e764f6384a88fefbe69bb3
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-04 08:33:07 +0800

    Testing Repeating String

--- leetcode/src/test/java/org/bank/BuddyString.java
+++ leetcode/src/test/java/org/bank/BuddyString.java
@@ -1,23 +1,41 @@
 package org.bank;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BuddyString {
   public static boolean buddyStrings(String s, String goal) {
     if (s.isEmpty() || s.length() == 1) return false;
     else if (s.length() != goal.length()) return false;
     else {
-      for (int i = 0; i < s.length(); i++) {
-        for (int j = i+1 ; j < s.length(); j++) {
-            char[] sChar = s.toCharArray();
-            char swap = sChar[i];
-            sChar[i] = sChar[j];
-            sChar[j] = swap;
-            String swapGoal = String.copyValueOf(sChar);
-            if (swapGoal.equals(goal)) {
-              return true;
-            }
+      return repeating(s, goal) || difference(s, goal);
+    }
+  }
+
+  static boolean repeating(String s, String goal) {
+    if (!s.equals(goal)) return false;
+    Set<Character> characters = new HashSet<>();
+    int bound = s.length();
+    for (int i = 0; i < bound; i++) {
+      Character charAt = s.charAt(i);
+      characters.add(charAt);
+    }
+    return goal.length() > characters.size(); // Implies there are recurring characters
+  }
+
+  static boolean difference(String s, String goal) {
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = i + 1; j < s.length(); j++) {
+        char[] sChar = s.toCharArray();
+        char swap = sChar[i];
+        sChar[i] = sChar[j];
+        sChar[j] = swap;
+        String swapGoal = String.copyValueOf(sChar);
+        if (swapGoal.equals(goal)) {
+          return true;
         }
       }
-      return false;
     }
+    return false;
   }
 }
