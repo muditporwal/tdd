5efe7b7fdddbb6df11f03bb9f08c3c11c830a2ec
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-12 09:18:31 +0800

    Updated getPossibleResults interface

--- leetcode/src/test/java/org/bank/PredictWinnerTest.java
+++ leetcode/src/test/java/org/bank/PredictWinnerTest.java
@@ -1,6 +1,10 @@
 package org.bank;
 
+import org.bank.PredictWinner.PlayResult;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,4 +41,20 @@ class PredictWinnerTest {
         b = PredictWinner.PredictTheWinner(nums);
         assertTrue(b);
     }
+
+    @Test
+    void playResultsForNoNumbers_ShouldReturnAnEmptyList() {
+        int[] nums = new int[0];
+        List<PlayResult> possibleResults = PredictWinner.getPossibleResults(nums, 0,nums.length-1);
+        Assertions.assertEquals(0, possibleResults.size());
+    }
+
+    @Test
+    void playResultsForOneNumber_ShouldReturnAListWithASingleElement() {
+        int[] nums = new int[]{1};
+        List<PlayResult> possibleResults = PredictWinner.getPossibleResults(nums, 0,nums.length-1);
+        Assertions.assertEquals(1, possibleResults.size());
+        Assertions.assertEquals(1, possibleResults.get(0).player1score());
+    }
+
 }
