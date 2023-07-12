784822a44a1c0c9b8b33ea6d8ac525ec0ec6aa1d
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-12 08:59:55 +0800

    Added Append to Score Method

--- leetcode/src/main/java/org/bank/PredictWinner.java
+++ leetcode/src/main/java/org/bank/PredictWinner.java
@@ -1,6 +1,5 @@
 package org.bank;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,16 +9,23 @@ class PredictWinner {
             //first choice is of player 1 and even if the two numbers are equal 1 still wins
             //No matter if it is 3 numbers the choice to player2 is only once
         else if (nums.length ==0) return false;
-        List<PlayResults> results = getPossibleResults(nums);
+        List<PlayResult> results = getPossibleResults(nums);
         return results.stream().anyMatch(p -> p.player1score >= p.player2score);
     }
 
-    private static List<PlayResults> getPossibleResults(int[] nums) {
-        PlayResults result = new PlayResults(4,3);
+    private static List<PlayResult> getPossibleResults(int[] nums) {
+        PlayResult result = new PlayResult(4,3);
         return Collections.singletonList(result);
     }
 
 
-    record PlayResults(int player1score ,int player2score) {
+    record PlayResult(int player1score , int player2score) {
+        PlayResult appendToPlayer1(int score){
+            return new PlayResult(player1score()+score,player2score());
+        }
+
+        PlayResult appendToPlayer2(int score){
+            return new PlayResult(player1score(),player2score()+score);
+        }
     }
 }
