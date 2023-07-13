cdfdec014a7b94b6fadacbd0f19c01de8aeffb9b
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-14 01:16:45 +0800

    Added logic to select options to not propagate a possibility when playing as player 2

--- leetcode/src/main/java/org/bank/PredictWinner.java
+++ leetcode/src/main/java/org/bank/PredictWinner.java
@@ -6,18 +6,20 @@ class PredictWinner {
   }
 
   static boolean getPossibleResults(
-      int[] nums, int left, int right, boolean player1, PlayResult parent) {
+      int[] nums, int left, int right, boolean isPlayer1, PlayResult parent) {
+    boolean currentPlayer = isPlayer1;
+    boolean nextPlayer = !isPlayer1;
     if (nums.length == 0) return true;
-    if (nums.length == 1 && player1)
-      return true; // If only 1 player gets to choose and it is player1's turn
+    if (nums.length == 1 && isPlayer1)
+      return true; // If only 1 player gets to choose and it is isPlayer1's turn
     if (right - left == 1) {
-      return resultForLast2Moves(player1, parent, nums[left], nums[right]).doesPlayer1Win();
+      return resultForLast2Moves(currentPlayer, parent, nums[left], nums[right]).doesPlayer1Win();
     } else {
       boolean leftResult =
-          getPossibleResults(nums, left + 1, right, !player1, parent.append(player1, nums[left]));
+          getPossibleResults(nums, left + 1, right, nextPlayer, parent.append(currentPlayer, nums[left]));
       boolean rightResult =
-          getPossibleResults(nums, left, right - 1, !player1, parent.append(player1, nums[right]));
-      return player1 && (leftResult || rightResult) || (!player1 && (leftResult && rightResult));
+          getPossibleResults(nums, left, right - 1, nextPlayer, parent.append(currentPlayer, nums[right]));
+      return isPlayer1 ? (leftResult || rightResult) : (leftResult && rightResult);
     }
   }
 
