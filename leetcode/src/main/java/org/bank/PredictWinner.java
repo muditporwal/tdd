7ad0f1771b77cb8ecf6a888e4ebd6bbe19cefdd0
Mudit Porwal <9289168+muditporwal@users.noreply.github.com>
2023-07-14 01:04:55 +0800

    Added logic to select options to not propagate a possibility when playing as player 2

--- leetcode/src/main/java/org/bank/PredictWinner.java
+++ leetcode/src/main/java/org/bank/PredictWinner.java
@@ -2,55 +2,38 @@ package org.bank;
 
 class PredictWinner {
   public static boolean PredictTheWinner(int[] nums) {
-    return getPossibleResults(nums, 0, nums.length - 1, true,PlayResult.newGame());
+    return getPossibleResults(nums, 0, nums.length - 1, true, PlayResult.newGame());
   }
 
   static boolean getPossibleResults(
-      int[] nums, int left, int right, boolean isPlayer1, PlayResult parent) {
+      int[] nums, int left, int right, boolean player1, PlayResult parent) {
     if (nums.length == 0) return true;
-    if (nums.length == 1 && isPlayer1)
+    if (nums.length == 1 && player1)
       return true; // If only 1 player gets to choose and it is player1's turn
     if (right - left == 1) {
-      return resultForLast2Moves(isPlayer1, parent, nums[left], nums[right]).doesPlayer1Win();
-    }
-    else {
-      if (getPossibleResults(nums,left+1,right,!isPlayer1, resultForMove(isPlayer1, parent, nums[left]))) return true;
-      if (getPossibleResults(nums,left,right-1,!isPlayer1, resultForMove(isPlayer1, parent, nums[right]))) return true;
-      return false;
-    }
-  }
-
-  private static PlayResult resultForLast2Moves(boolean isPlayer1, PlayResult parent, int l, int r) {
-    if (r >= l) {
-      PlayResult res = resultForMove(isPlayer1, parent, r);
-      return resultForMove(!isPlayer1, res, l); // toggle player for move
+      return resultForLast2Moves(player1, parent, nums[left], nums[right]).doesPlayer1Win();
     } else {
-      PlayResult res = resultForMove(isPlayer1, parent, l);
-      return resultForMove(!isPlayer1, res, r); // toggle player for move
+      boolean leftResult =
+          getPossibleResults(nums, left + 1, right, !player1, parent.append(player1, nums[left]));
+      boolean rightResult =
+          getPossibleResults(nums, left, right - 1, !player1, parent.append(player1, nums[right]));
+      return player1 && (leftResult || rightResult) || (!player1 && (leftResult && rightResult));
     }
   }
 
-  private static PlayResult resultForMove(boolean isPlayer1, PlayResult parent, int score) {
-    if (isPlayer1) {
-      return parent.appendToPlayer1(score);
-    }
-    else {
-      return parent.appendToPlayer2(score);
-    }
+  private static PlayResult resultForLast2Moves(boolean player1, PlayResult parent, int l, int r) {
+    if (r >= l) return parent.append(player1, r).append(!player1, l);
+    else return parent.append(player1, l).append(!player1, r);
   }
-
 
   record PlayResult(int player1score, int player2score) {
     public static PlayResult newGame() {
       return new PlayResult(0, 0);
     }
 
-    PlayResult appendToPlayer1(int score) {
-      return new PlayResult(player1score() + score, player2score());
-    }
-
-    PlayResult appendToPlayer2(int score) {
-      return new PlayResult(player1score(), player2score() + score);
+    PlayResult append(boolean isPlayer1, int score) {
+      if (isPlayer1) return new PlayResult(player1score() + score, player2score());
+      else return new PlayResult(player1score(), player2score() + score);
     }
 
     public boolean doesPlayer1Win() {
